2f062eab84f1da790faba17bdc67bff6a78e3a63
Gerald Yeo <gerald@fusedthought.com>
2018-08-19 00:37:27 +0800

    chore: cleanup ci script and unused constants

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,2 @@
-export const DEVTOOLS_REGISTER = '@@RWM/REGISTER';
-export const DEVTOOLS_INIT = '@@RWM/INIT';
 export const BATCH_ACTION = '@@RWM/BATCH_ACTION';
 export const SET_ACTION = '@@RWM/SET_ACTION';
